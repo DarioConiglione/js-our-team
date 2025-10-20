fdd1a2ddd58ab9cc658eaeb7213f730bc896e4e2
DarioConiglione <dariocng13@gmail.com>
2025-10-20 16:50:02 +0100

    logging variables with all columns

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,9 +43,43 @@ console.log("it works");
 const cardEl = document.querySelector("#members, .card");
 console.log(cardEl);
 
+let colsStrings = ''
+
+
+
+// LOOP
+
 for (let i = 0; i < teamMembers.length; i++) {
   const thisMember = teamMembers[i];
   console.log(thisMember);
-  
+
+  const {name, role, email, img} = thisMember;
+  console.log(name, role, email, img);
+
+  const markupString = `
+  <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="./assets/img/${img}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <p class="card-text">${role}</p>
+        <p class="card-text"><small class="text-body-secondary">${email}</small></p>
+      </div>
+    </div>
+  </div>
+</div>`
+
+
+colsStrings += markupString;
+
 }
+
+
+
+
+console.log(colsStrings);
+
 
